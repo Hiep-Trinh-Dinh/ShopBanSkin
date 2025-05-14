4d8695975e4937730957815788f89ec3a8515d9c
Enryu <you@example.com>
2025-05-14 21:34:28 +0700

    fix

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,11 @@
 import Header from './Header.jsx';
 import ImageCarousel from './ChuyenHinhAnh.jsx';
-import AI from './AI.jsx';
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
       <ImageCarousel />
-      <AI />
     </main>
   );
 }
